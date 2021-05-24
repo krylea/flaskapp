6e2a1be03d65cf9c040bb7cafce7b35d1437af01
Kira Selby <kaselby@uwaterloo.ca>
2021-05-24 05:32:06 -0400

    updates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,29 @@
+from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-    return "Hello, World!"
+    user = {'username': 'Kira'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Javert'},
+            'body': 'Valjean! At last! We see each other plain. Monsieur le Mayor, you wear a different chain.'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
